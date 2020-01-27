2eefc2c45c164d7d47caa51a7e3685b027d80c70
Elena Kulakova <egeorgievska89@gmail.com>
2020-01-27 10:41:35 +0100

    Initial Commit

---
+++ WKWebViewAndUIAlertCOntroller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WKWebViewAndUIAlertCOntroller
+//
+//  Created by Elena Kulakova on 2020-01-27.
+//  Copyright © 2020 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
